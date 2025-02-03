7e0240a32d526f6089475b10469e9df95c02e58f
gilmar-sales <gilmar.custodio@outlook.com.br>
2025-02-03 20:39:51 -0300

    Updating Freyr

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,23 +6,32 @@
 #include "Components/ModelComponent.hpp"
 #include "Components/PlayerComponent.hpp"
 #include "Components/RigidBodyComponent.hpp"
+#include "Components/SpaceShipControlComponent.hpp"
 #include "Components/SphereColliderComponent.hpp"
 #include "Components/TransformComponent.hpp"
-
-#include <Components/SpaceShipControlComponent.hpp>
 
 #include "Systems/CollisionSystem.hpp"
 #include "Systems/InputSystem.hpp"
 #include "Systems/MovementSystem.hpp"
 #include "Systems/PhysicsSystem.hpp"
+#include "Systems/PlayerCameraSystem.hpp"
+#include "Systems/PlayerControlSystem.hpp"
 #include "Systems/RenderSystem.hpp"
 #include "Systems/SpawnSystem.hpp"
-#include <Systems/PlayerCameraSystem.hpp>
-#include <Systems/PlayerControlSystem.hpp>
 
 int main(int argc, char const* argv[])
 {
-    auto builder = fra::ApplicationBuilder();
+    auto builder =
+        fra::ApplicationBuilder()
+            .WithWindow([](fra::WindowBuilder& windowBuilder) {
+                windowBuilder.SetWidth(1920).SetHeight(1080).SetVSync(false);
+            })
+            .WithRenderer([](fra::RendererBuilder& rendererBuilder) {
+                rendererBuilder.SetSamples(vk::SampleCountFlagBits::e8)
+                    .SetClearColor({ 0.0f, 0.0f, 0.0f, 0.0f })
+                    .SetDrawDistance(100'000.0f)
+                .SetSamples(vk::SampleCountFlagBits::e1);
+            });
 
     builder.GetServiceCollection()->AddSingleton<fr::Scene>(
         [&](ServiceProvider& serviceProvider) {
@@ -44,16 +53,6 @@ int main(int argc, char const* argv[])
                 .AddSystem<RenderSystem>()
                 .SetMaxEntities(2'100'000)
                 .Build(serviceProvider);
-        });
-
-    builder
-        .WithWindow([](fra::WindowBuilder& windowBuilder) {
-            windowBuilder.SetWidth(1920).SetHeight(1080).SetVSync(false);
-        })
-        .WithRenderer([](fra::RendererBuilder& rendererBuilder) {
-            rendererBuilder.SetSamples(vk::SampleCountFlagBits::e8)
-                .SetClearColor({ 0.0f, 0.0f, 0.0f, 0.0f })
-                .SetDrawDistance(100'000.0f);
         });
 
     const auto app = builder.Build<SpaceApp>();
