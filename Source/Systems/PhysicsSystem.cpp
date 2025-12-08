a44c450c5fca3e67d96e4d8c56356dd33dcae9d3
gilmar-sales <gilmar.custodio@outlook.com.br>
2025-12-08 19:35:06 -0300

    fix: position change setting NaN values

--- Source/Systems/PhysicsSystem.cpp
+++ Source/Systems/PhysicsSystem.cpp
@@ -12,26 +12,23 @@ PhysicsSystem::PhysicsSystem(const Ref<fr::Scene>& scene) : System(scene)
         mScene->AddEventListener<CollisionEvent>([scene = mScene](const CollisionEvent collisionEvent) {
             scene->TryGetComponents<TransformComponent, SphereColliderComponent, RigidBodyComponent>(
                 collisionEvent.target,
-                [&](TransformComponent& targetTransform, SphereColliderComponent& targetCollider,
+                [&](TransformComponent& targetTransform, const SphereColliderComponent& targetCollider,
                     RigidBodyComponent& targetRigidBody) {
                     scene->TryGetComponents<TransformComponent, SphereColliderComponent, RigidBodyComponent>(
                         collisionEvent.collisor,
-                        [&](TransformComponent& collisorTransform, SphereColliderComponent& collisorCollider,
+                        [&](TransformComponent& collisorTransform, const SphereColliderComponent& collisorCollider,
                             RigidBodyComponent& collisorRigidBody) {
                             const auto distance = glm::distance(targetTransform.position, collisorTransform.position);
 
                             const auto totalForce = (targetCollider.radius + collisorCollider.radius - distance);
 
-                            const auto targetForce =
-                                totalForce * (collisorRigidBody.mass / (targetRigidBody.mass + collisorRigidBody.mass));
+                            const auto totalMass = targetRigidBody.mass + collisorRigidBody.mass;
 
-                            const auto collisorForce =
-                                totalForce * (targetRigidBody.mass / (targetRigidBody.mass + collisorRigidBody.mass));
+                            const auto targetForce = totalForce * (collisorRigidBody.mass / totalMass);
+
+                            const auto collisorForce = totalForce * (targetRigidBody.mass / totalMass);
 
                             const auto direction = normalize(targetTransform.position - collisorTransform.position);
-
-                            targetTransform.position += direction * targetForce;
-                            collisorTransform.position += -direction * collisorForce;
 
                             targetRigidBody.ApplyForce(direction, targetForce, collisionEvent.deltaTime);
                             collisorRigidBody.ApplyForce(-direction, collisorForce, collisionEvent.deltaTime);
