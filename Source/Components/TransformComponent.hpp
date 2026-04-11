6a8512f1baada1e5bc265b06dcaf83ed15a591f3
gilmar-sales <gilmar.custodio@outlook.com.br>
2026-04-11 00:40:16 -0300

    refactor! transform: add default initializers, constexpr directions, and normalize rotations

--- Source/Components/TransformComponent.hpp
+++ Source/Components/TransformComponent.hpp
@@ -7,31 +7,34 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-struct TransformComponent : fr::Component
-{
-    glm::vec3 position;
-    glm::quat rotation;
-    glm::vec3 scale;
+struct TransformComponent : fr::Component {
+    glm::vec3 position{glm::vec3(0.0f)};
+    glm::quat rotation{glm::identity<glm::quat>()};
+    glm::vec3 scale{glm::vec3(1.0f)};
 
-    glm::vec3 GetForwardDirection() const { return glm::vec3(0, 0, 1) * rotation; }
+    static constexpr glm::vec3 FORWARD{0.0f, 0.0f, 1.0f};
+    static constexpr glm::vec3 RIGHT{1.0f, 0.0f, 0.0f};
+    static constexpr glm::vec3 UP{0.0f, 1.0f, 0.0f};
 
-    glm::vec3 GetRightDirection() const { return glm::vec3(1, 0, 0) * rotation; }
+    [[nodiscard]] glm::vec3 GetForwardDirection() const { return FORWARD * rotation; }
+    [[nodiscard]] glm::vec3 GetRightDirection() const { return RIGHT * rotation; }
+    [[nodiscard]] glm::vec3 GetUpDirection() const { return UP * rotation; }
 
-    glm::vec3 GetUpDirection() const { return glm::vec3(0, 1, 0) * rotation; }
-
-    glm::mat4 GetModel() const
-    {
-        const auto translateM = glm::translate(glm::mat4(1), position);
-        const auto rotateM    = glm::mat4(glm::inverse(rotation));
-        const auto scaleM     = glm::scale(glm::mat4(1), scale);
-
-        return translateM * rotateM * scaleM;
+    [[nodiscard]] glm::mat4 GetModel() const {
+        return glm::translate(glm::identity<glm::mat4>(), position) *
+               glm::mat4(glm::inverse(rotation)) *
+               glm::scale(glm::identity<glm::mat4>(), scale);
     }
 
-    void Rotate(const glm::vec3& axis, float angle, float deltaTime)
-    {
-        const auto angularAcceleration = glm::radians(angle * deltaTime);
+    void Rotate(const glm::vec3 &axis, const float angleDegPerSec, const float deltaTime) {
+        const float deltaAngle = glm::radians(angleDegPerSec) * deltaTime;
 
-        rotation *= glm::angleAxis(angularAcceleration, axis);
+        rotation = glm::normalize(rotation * glm::angleAxis(deltaAngle, axis));
+    }
+
+    void Reset() {
+        position = glm::vec3(0.0f);
+        rotation = glm::identity<glm::quat>();
+        scale = glm::vec3(1.0f);
     }
 };
